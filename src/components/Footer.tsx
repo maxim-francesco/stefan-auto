1747338564c68e6628f86ce745ddfa876e21a881
Francesco Maxim <maaximfrancesco@gmail.com>
2026-01-28 13:10:56 +0000

    poti sa folosesti react-icons pentru social media ?

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 
 import { Link } from "react-router-dom";
-import { Phone, Mail, MapPin, Clock, MessageCircle, Facebook } from "lucide-react";
+import { Phone, MapPin, MessageCircle } from "lucide-react";
 import logo from "@/assets/Stefan.png";
+import { FaTiktok, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -107,11 +108,11 @@ const Footer = () => {
                 <h5 className="font-display text-base mb-4 text-gold-gradient">Ne găsiți și pe:</h5>
                 <div className="flex flex-col items-start gap-3">
                      <a href="https://www.tiktok.com/@auto.sh.rate.avans.0?_r=1&_t=ZN-93Ru6FYY01e" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-colors flex items-center gap-2 text-sm">
-                        <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-2.43.05-4.85-.38-6.75-1.9-1.42-1.13-2.2-2.7-2.4-4.35-.02-.24-.03-.48-.03-.73v-5.04c.25 0 .5 0 .74 0 1.13 0 2.25-.01 3.38-.02 1.5-.01 2.99-.01 4.49-.01.02-3.46.02-6.92.01-10.38z"></path></svg>
+                        <FaTiktok className="w-5 h-5" />
                         <span>auto.sh.rate.avans.0</span>
                     </a>
                     <a href="https://www.facebook.com/share/18K1aijRxX/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-colors flex items-center gap-2 text-sm">
-                        <Facebook className="w-5 h-5" />
+                        <FaFacebook className="w-5 h-5" />
                         <span>Stefan auto-gvr</span>
                     </a>
                 </div>
